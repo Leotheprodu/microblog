3c2b7d8684eee7464de8133450eef56f52702d34
Leonardo Serrano Alfaro <leovpc@gmail.com>
2024-06-13 00:41:40 -0600

    chore: Update dependencies and code formatting

---
+++ src/components/global/constants.ts
@@ -0,0 +1,9 @@
+export const blogName = "Blog Sadico";
+export const blogDescription = "Blog Sadico";
+export const heroTitle = "Este blog me va a hacer rico";
+export const lang = "es";
+export const footerDescription = "Todos los derechos reservados.";
+export const ctaDescription =
+  "Únete para recibir nuestras actualizaciones semanales";
+export const ctaButtonText = "suscribirme";
+export const ctaPlaceholderText = "Tu correo electrónico";
